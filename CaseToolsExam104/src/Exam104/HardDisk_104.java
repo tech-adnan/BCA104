8848a4fcb1615d66b8f26b235cd03649c7ff63c5
exam <exam@Bcalab4-m04>
2023-05-03 10:43:59 +0530

    Second Commit

--- CaseToolsExam104/src/Exam104/HardDisk_104.java
+++ CaseToolsExam104/src/Exam104/HardDisk_104.java
@@ -1,12 +1,58 @@
 package Exam104;
 
+    /**
+     * @author exam104
+     * this is a class called Hard disk <p>
+     * this class is used to determine the size of hardDisk and to format and delete it.
+     * this class has three things to do:-
+     * 1)format the HardDisk
+     * 2)Delete it
+     * 3)Determine the size
+     */
+
 public class HardDisk_104 {
 	
 	int size;
 	
+	/**
+	 * 
+	 * @param size
+	 * This is a constructor used to initialize the class HardDisk
+	 * 
+	 */
 	HardDisk_104(int size)
 	{
 		this.size=size;
+	}
+	
+	/**
+	 * This is a function used to determine Format of hardDisk
+	 * @see Format
+	 */
+	
+	void format()
+	{
+		size = 1000;
+	}
+	
+	/**
+	 * This is a function used to Delete file size from size
+	 * @param file_size
+	 */
+	
+	void delete(int file_size)
+	{
+		size = size - file_size;
+	}
+	
+	/**
+	 * This is a Function used to display the remaining memory in HardDisk
+	 * @see +size
+	 */
+	
+	void display_()
+	{
+		System.out.println("The remaining memory is " +size);
 	}
 
 	public static void main(String[] args) {
